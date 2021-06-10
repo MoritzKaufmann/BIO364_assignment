5003f8bca750375aca2c5fb383914d7894fe051c
MoritzKaufmann <37136445+MoritzKaufmann@users.noreply.github.com>
2021-06-10 11:37:58 +0200

    Update run_dashboard.R

--- run_dashboard.R
+++ run_dashboard.R
@@ -1,3 +1,5 @@
+#This script is part of the asignment for BIO 364: The physics of life from the university of Zürich
+#The script was written by Moritz Kaufmann 07.06.21
 #install the needed apps for the dashboard if they are not already installed
 list.of.packages <- c("shiny", "ggplot2", "data.table", "dplyr", "tidyr", "Rfast")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
